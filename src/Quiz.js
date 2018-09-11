b883d9c0c0cde6dc0384e612fa28f016427545e3
Heather Palmergit config --global user.email heatherpa@zillowgroup.com <palmerheather78@gmail.com>
2018-09-11 08:02:52 -0700

    Add first question

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 let quizData = require('./quiz_data.json');
 
-create Class extends Component {
+class Quiz extends Component {
   constructor(props){
     super(props)
-    this.state = {quiz_position: 1}
+    this.state = { quiz_position: 1 }
   }
   render(){
     return(
       <div>
-        <div className='quizQuestion'>
-
+        <div className="QuizQuestion">
+          {quizData.quiz_questions[0].instruction_text}
         </div>
       </div>
     )
