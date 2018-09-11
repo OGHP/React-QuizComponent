2e64d6ef93aca36494bace8fc0e9da61eccd321c
Heather Palmergit config --global user.email heatherpa@zillowgroup.com <palmerheather78@gmail.com>
2018-09-11 13:08:32 -0700

    Add event handlers

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 
 class QuizQuestionButton extends Component {
+  handleClick(onclick, button) {
+    //calls the handle click method in the QuizQuestion component whenever a button is pressed and passes that button text value to that method
+    this.props.clickHandler(this.props.button_text)
+  }
   render () {
     return (
-      <li><button>{ this.props.button_text }</button></li>
+      <li><button onClick={this.handleClick.bind(this)}>{ this.props.button_text }</button></li>
     )
   }
 }
